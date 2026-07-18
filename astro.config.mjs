9d71a25f9692d21d481cde0cad2f5ab631b9f588
varsanraja5-cmd <varsanraja5@gmail.com>
2026-07-18 14:04:07 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://varsanraja5-cmd.github.io/varsan_website',
+  site: 'https://varsanraja5-cmd.github.io',
   base: '/varsan_website'
   //integrations: [mdx(), sitemap(), tailwind()]
 });
