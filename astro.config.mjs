0235f1760bfb6a9e724bb2ffbe6248f68bc75b68
varsanraja5-cmd <varsanraja5@gmail.com>
2026-07-18 12:43:55 -0500

    Updating everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://varsanraja5-cmd.github.io',
+  base: '/varsan_website',
   integrations: [mdx(), sitemap(), tailwind()]
 });
